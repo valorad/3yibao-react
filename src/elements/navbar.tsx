d8e1780f066c1491a65ab61520cb1962839be6ed
valorad <valorad@outlook.com>
2019-05-08 14:44:04 -0400

    navbar

---
+++ src/elements/navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {
+  Link
+} from "react-router-dom";
+
+import "./navbar.scss";
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <section className="navbar">
+        <nav>
+          <h1>3yibao React</h1>
+          <main>
+            <ul>
+              <li>
+                <Link to="/index">index</Link>
+              </li>
+              <li>
+                <Link to="/404">404</Link>
+              </li>
+            </ul>
+          </main>
+          <div className="flexSpacer"></div>
+          <footer>
+            <ul>
+              <li>
+                <a href="##">Github</a>
+              </li>
+            </ul> 
+          </footer>
+        </nav>
+      </section>
+    );
+  }
+}
