3d1d6c172ae6ecc7fc7638373f62871e8b9b226d
valorad <valorad@outlook.com>
2019-05-16 14:22:18 -0400

    add 404 page

--- src/components/http404.tsx
+++ src/components/http404.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import {
+  Link
+} from "react-router-dom";
+
+import "./http404.scss";
+
 export default class Http404 extends React.Component {
 
   render() {
     return (
         <section className="http404">
-          404 works
+        <div>
+          <h1>404 Page Not Found</h1>
+          <div>
+            <p>很抱歉让您走丢了，快回到 </p>
+            <p><Link to="/index"> 首页</Link></p>
+          </div>
+        </div>
         </section>
     );
   }
